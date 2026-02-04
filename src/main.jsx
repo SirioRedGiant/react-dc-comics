faffbd38da7e9839f98a18baaed2f93f89afb570
SirioRedGiant <Oiris1992@gmail.com>
2026-02-04 15:39:50 +0100

    Rinominata repository

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+// Import Bootstrap
+import "node_modules/bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap";
+
+// Import Open Sans => Font utilizzato
+
+
+// Import local assets
 import "./assets/css/index.css";
+
+// Import root component
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
