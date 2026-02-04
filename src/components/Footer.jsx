46aac41e1812f3d9c0bf1cae7f6f0161e9ec3c15
SirioRedGiant <Oiris1992@gmail.com>
2026-02-04 18:13:13 +0100

    test icone nel footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,16 @@
+import digitalComicsIcon from "../assets/img/buy-comics-digital-comics.png"
+
+
+
 export default function Footer() {
-  return <footer>footer</footer>;
+  return <footer>
+    <div className="bg-primary py-5">
+      <div className="container d-flex justify-content-around align-items-center">
+        <div className="d-flex align-items-center text-white">
+          <img src={digitalComicsIcon} alt="" />
+        </div>
+
+      </div>
+    </div>
+  </footer>;
 }
