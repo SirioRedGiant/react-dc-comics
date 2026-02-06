4adcf35fb308443e406f41e65d54092881efb39b
SirioRedGiant <Oiris1992@gmail.com>
2026-02-06 15:29:33 +0100

    utilizzo di props per migliorare lettura e riutilizzabilità

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
+//note ARRAY COMICS fumetti
+import comicsListData from "./data/comics";
+
 export default function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Main comics={comicsListData} />
       <Footer />
     </>
   );
