71f5a8e23ccf86c369b41ead17e810537644fe0f
SirioRedGiant <Oiris1992@gmail.com>
2026-02-07 12:21:40 +0100

    approching footer link lists

---
+++ src/data/footerlinks.js
@@ -0,0 +1,93 @@
+ export default [
+ 
+ const infoBuyLinks = {[
+    {
+      img: digitalComicsIcon,
+      text: "DIGITAL COMICS",
+      url: "#",
+    },
+    {
+      img: merchandiseIcon,
+      text: "DC MERCHANDISE",
+      url: "#",
+    },
+    {
+      img: subscriptionIcon,
+      text: "SUBSCRIPTION",
+      url: "#",
+    },
+    {
+      img: comicShopLocator,
+      text: "COMIC SHOP LOCATOR",
+      url: "#",
+    },
+    {
+      img: dcPowerVisa,
+      text: "DC POWER VISA",
+      url: "#",
+    },
+  ];
+ }
+  //note HERO fascia con gli eroi
+  const heroSections = [
+    {
+      title: "DC COMICS",
+      texts: [
+        "Characters",
+        "comics",
+        "Movies",
+        "TV",
+        "Games",
+        "Videos",
+        "News",
+      ],
+    },
+    {
+      title: "SHOP",
+      texts: ["Shop DC", "Shop DC Collectibles"],
+    },
+    {
+      title: "DC",
+      texts: [
+        "Term Of Use",
+        "Privacy policy (New)",
+        "Ad choices",
+        "Advertising",
+        "Jobs",
+        "Subscriptions",
+        "Talent Workshops",
+        "CPSC Certificates",
+        "Ratings",
+        "Shop Help",
+        "Contact Us",
+      ],
+    },
+    {
+      title: "SITES",
+      texts: ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Per Visa"],
+    },
+  ];
+
+  //* DATI piè pagina social
+  const socialLinks = [
+    {
+      img: facebookLink,
+      url: "#",
+    },
+    {
+      img: twitterLink,
+      url: "#",
+    },
+    {
+      img: youtubeLink,
+      url: "#",
+    },
+    {
+      img: pinterestLink,
+      url: "#",
+    },
+    {
+      img: periscopeLink,
+      url: "#",
+    },
+  ];
